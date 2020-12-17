e84e27306e228dbf8cfba2b5dd08a677f2f7ade3
biangomes <biangomes0707@gmail.com>
2020-12-17 17:17:28 -0500

    Adicionando conteúdo

---
+++ php/curso-php/exercicio.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@200&display=swap" rel="stylesheet">
+	<link href="assets/css/estilo.css" rel="stylesheet">
+	<link href="assets/css/exercicio.css" rel="stylesheet">
+	<title>Exercício</title>
+</head>
+
+<body class="exercicio">
+	<header class="cabecalho">
+		<h1>Curso PHP</h1>
+		<h2>Visualização do Exercício</h2>
+	</header>
+	
+	<nav class="navegacao">
+		<a href="#" class="verde">Sem formatação</a>
+		<a href="index.php" class="vermelho">Voltar</a>
+	</nav>
+
+	<main class="principal">
+		<div class="conteudo">
+			
+		</div>	
+	</main>
+	
+	<footer class="rodape">
+		COD3R & ALUNOS ₢ <?= date("Y"); ?>
+	</footer>
+
+</body>
+
+</html>
---
+++ php/curso-php/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@200&display=swap" rel="stylesheet">
+	<link href="assets/css/estilo.css" rel="stylesheet">
+	<title>Curso PHP</title>
+</head>
+
+<body>
+	<header class="cabecalho">
+		<h1>Curso PHP</h1>
+		<h2>Índice dos exercícios</h2>
+	</header>
+	
+	<main class="principal">
+		<div class="conteudo">
+			<nav class="modulos">
+				<div class="modulo verde">
+					<h3>Módulo 01</h3>
+					<ul>
+						<li><a href="exercicio.php">Exercício A</a></li>
+						<li><a href="#">Exercício B</a></li>
+						<li><a href="#">Exercício C</a></li>
+					</ul>	
+				</div>
+
+				<div class="modulo vermelho">
+					<h3>Módulo 02</h3>
+					<ul>
+						<li><a href="#">Exercício A</a></li>
+						<li><a href="#">Exercício B</a></li>
+						<li><a href="#">Exercício C</a></li>
+					</ul>	
+				</div>	
+
+				<div class="modulo azul">
+					<h3>Módulo 03</h3>
+					<ul>
+						<li><a href="#">Exercício A</a></li>
+						<li><a href="#">Exercício B</a></li>
+						<li><a href="#">Exercício C</a></li>
+					</ul>	
+				</div>
+
+				<div class="modulo roxo">
+					<h3>Módulo 04</h3>
+					<ul>
+						<li><a href="#">Exercício A</a></li>
+						<li><a href="#">Exercício B</a></li>
+						<li><a href="#">Exercício C</a></li>
+					</ul>	
+				</div>
+
+				<div class="modulo laranja">
+					<h3>Módulo 05</h3>
+					<ul>
+						<li><a href="#">Exercício A</a></li>
+						<li><a href="#">Exercício B</a></li>
+						<li><a href="#">Exercício C</a></li>
+					</ul>	
+				</div>
+
+				<div class="modulo verde-escuro">
+					<h3>Módulo 06</h3>
+					<ul>
+						<li><a href="#">Exercício A</a></li>
+						<li><a href="#">Exercício B</a></li>
+						<li><a href="#">Exercício C</a></li>
+					</ul>	
+				</div>
+
+				<div class="modulo vermelho-escuro">
+					<h3>Módulo 07</h3>
+					<ul>
+						<li><a href="#">Exercício A</a></li>
+						<li><a href="#">Exercício B</a></li>
+						<li><a href="#">Exercício C</a></li>
+					</ul>	
+				</div>
+
+				<div class="modulo azul-escuro">
+					<h3>Módulo 08</h3>
+					<ul>
+						<li><a href="#">Exercício A</a></li>
+						<li><a href="#">Exercício B</a></li>
+						<li><a href="#">Exercício C</a></li>
+					</ul>	
+				</div>
+
+				<div class="modulo roxo-escuro">
+					<h3>Módulo 09</h3>
+					<ul>
+						<li><a href="#">Exercício A</a></li>
+						<li><a href="#">Exercício B</a></li>
+						<li><a href="#">Exercício C</a></li>
+					</ul>	
+				</div>
+			</nav>
+		</div>	
+	</main>
+	
+	<footer class="rodape">
+		COD3R & ALUNOS ₢ <?= date("Y"); ?>
+	</footer>
+
+</body>
+
+</html>
