394d0633f001b63d3f32191f2dc69ef9dc2e32b5
biangomes <biangomes0707@gmail.com>
2021-03-30 19:10:10 -0500

    add

--- Ecommerce/pedido/models.py
+++ Ecommerce/pedido/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Pedido(models.Model):
-    
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField()
