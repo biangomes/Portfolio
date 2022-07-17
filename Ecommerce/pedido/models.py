fdf93694743ba3a69f435e978ba5e8fb16b28423
Beatriz Gomes <biangomes0707@gmail.com>
2022-07-17 17:49:02 -0300

    Delete Ecommerce directory

--- Ecommerce/pedido/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class Pedido(models.Model):
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    total = models.FloatField()
